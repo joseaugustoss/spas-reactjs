dd88725411967ed56b2ff1756d7887cace722290
José Augusto <joseaugusto0408@gmail.com>
2022-07-21 10:38:54 -0300

    pagina home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,36 @@
+import { Play } from "phosphor-react";
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from "./styles";
+
 export function Home() {
   return (
-    <>
-      <h1>Home</h1>
-    </>
-  )
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" placeholder=" " />
+
+          <label htmlFor="minutesAmount">Durante</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </FormContainer>
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+        <button type="submit">
+          <Play size={24} />
+          Comerçar
+        </button>
+      </form>
+    </HomeContainer>
+  );
 }
